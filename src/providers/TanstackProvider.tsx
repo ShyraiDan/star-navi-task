02d00d123eb434f936a9d499af7761a4475bb560
ShyraiDan <dan.shirayy@gmail.com>
2025-11-07 22:48:52 +0200

    Chore: Added docs for providers

--- src/providers/TanstackProvider.tsx
+++ src/providers/TanstackProvider.tsx
@@ -5,6 +5,11 @@ interface TanstackProviderProps {
   children: ReactNode
 }
 
+/**
+ * A React Query provider that wraps the application with a QueryClient.
+ * @param {TanstackProviderProps} props - The props for the TanstackProvider component.
+ * @returns {JSX.Element} - The rendered QueryClientProvider component.
+ */
 const TanstackProvider = ({ children }: TanstackProviderProps) => {
   const [queryClient] = useState(() => new QueryClient())
 
